b7568c21377873d9165be2636448c3cb5ed68d4e
MOHAMEDELSHEKHA <mohamedelshekha0@gmail.com>
2025-04-28 21:29:30 +0300

    first commit

---
+++ BookingService/src/main/java/com/example/hotel/BookingService/Services/BookingService.java
@@ -0,0 +1,29 @@
+package com.example.hotel.BookingService.Services;
+
+import com.example.hotel.BookingService.Clients.AvailabilityClient;
+import com.example.hotel.BookingService.rabbitmq.BookingProducer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
+
+@Service
+public class BookingService {
+    private final AvailabilityClient availability;
+    private final BookingProducer producer;
+
+    @Value("${Name}")
+    String name;
+
+    @Value("${ID}")
+    String id;
+
+    public BookingService(AvailabilityClient availability,
+                          BookingProducer producer) {
+        this.availability = availability;
+        this.producer     = producer;
+    }
+}
+
